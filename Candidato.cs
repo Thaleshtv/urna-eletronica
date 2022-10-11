982a89b2048245e26cbcf136cafedeab2685d001
Thaleshtv <thalesviegas8@gmail.com>
2022-10-10 21:45:30 -0300

    Cria urna eletronica

---
+++ Candidato.cs
@@ -0,0 +1,74 @@
+using System.Runtime.Intrinsics.X86;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica
+{
+    public class CandidatoLula
+    {
+        public int NumeroCandidatoLula { get; set; }
+        public string? NomeCandidatoLula { get; set; }
+        public int NumeroDeVotosCandidatoLula { get; set; }
+        public CandidatoLula(){
+            NumeroCandidatoLula = 13;
+            NomeCandidatoLula = "Lula";
+            NumeroDeVotosCandidatoLula = 0;
+        }  
+
+        public void SetNumeroDeVotosCandidatoLula(int value) {
+            NumeroDeVotosCandidatoLula = NumeroDeVotosCandidatoLula + 1;
+        }
+
+        public int GetNumeroDeVotosCandidatoLula() {
+            return NumeroDeVotosCandidatoLula;
+        }
+    }
+
+    public class CandidatoBolsonaro 
+    {
+        public int NumeroCandidatoBolsonaro { get; set; }
+        public string? NomeCandidatoBolsonaro { get; set; }
+
+        public int NumeroDeVotosCandidatoBolsonaro { get; set; }
+
+        public CandidatoBolsonaro() {
+            NumeroCandidatoBolsonaro = 22;
+            NomeCandidatoBolsonaro = "Bolsonaro"; 
+            NumeroDeVotosCandidatoBolsonaro = 0;
+        }
+
+        public void SetNumeroDeVotosCandidatoBolsonaro(int value) {
+            NumeroDeVotosCandidatoBolsonaro = NumeroDeVotosCandidatoBolsonaro + value;
+        }
+
+        public int GetNumeroDeVotosCandidatoBolsonaro() {
+            return NumeroDeVotosCandidatoBolsonaro;
+        }
+    }
+
+    public class VotoNulo
+    {
+        public int NumeroVotoNulo { get; set; }
+        public string? NomeVotoNulo { get; set; }
+        public int NumeroDeVotosNulo { get; set; }
+
+        public VotoNulo()
+        {
+            NumeroVotoNulo = 0;
+            NomeVotoNulo = "Nulo";
+            NumeroDeVotosNulo = 0;
+        }
+
+        public void SetVotoNulo(int value)
+        {
+            NumeroDeVotosNulo = NumeroDeVotosNulo + value;
+        }
+
+        public int GetNumeroDeVotosNulo()
+        {
+            return NumeroDeVotosNulo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaEletronica
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            CandidatoLula candidatoUm = new CandidatoLula();
+            CandidatoBolsonaro candidatoDois = new CandidatoBolsonaro();
+            VotoNulo votoNulo = new VotoNulo();
+
+            Console.WriteLine("Bolsonaro - 22 \r\nLula - 13\r\nNulo - 0\r\n");
+
+            for(int i = 0; i < 5; i++) {
+                Console.WriteLine("Digite o numero do seu candidato\r\n");
+
+                string? numeroCandidato = Console.ReadLine();
+
+                if(numeroCandidato == "13") {
+                    candidatoUm.SetNumeroDeVotosCandidatoLula(1);
+                    Console.WriteLine("Você votou em Lula\r\n");
+                }
+
+                if(numeroCandidato == "22") {
+                    candidatoDois.SetNumeroDeVotosCandidatoBolsonaro(1);
+                    Console.WriteLine("Você votou em Bolsonaro\r\n");
+                }
+
+                if(numeroCandidato == "0") {
+                    votoNulo.SetVotoNulo(1);
+                    Console.WriteLine("Você votou nulo!\r\n");
+                }
+
+                if(numeroCandidato != "13" && numeroCandidato != "22" && numeroCandidato != "0") {
+                    Console.WriteLine("Numero de candidato inexistente, voto não computado!\r\n");
+                }
+            }
+            
+            string presidenteEleito = "";
+            int numeroDeVotosBolsonaro = candidatoDois.GetNumeroDeVotosCandidatoBolsonaro();
+            int numeroDeVotosLula = candidatoUm.GetNumeroDeVotosCandidatoLula();
+            int numeroDeVotosNulo = votoNulo.GetNumeroDeVotosNulo();
+
+            if(numeroDeVotosBolsonaro > numeroDeVotosLula) {
+                presidenteEleito = "Bolsonaro";
+            } 
+
+            if(numeroDeVotosLula > numeroDeVotosBolsonaro) {
+                presidenteEleito = "Lula";
+            }
+
+            if(numeroDeVotosBolsonaro == numeroDeVotosLula) {
+                presidenteEleito = "Empate!";
+            }
+            
+
+            Console.WriteLine($"Apuração total dos votos! \r\nBolsonaro - {candidatoDois.GetNumeroDeVotosCandidatoBolsonaro()} \r\nLula - {candidatoUm.GetNumeroDeVotosCandidatoLula()} \r\nNulo - {votoNulo.GetNumeroDeVotosNulo()} \r\n\nPresidente eleito - {presidenteEleito}");
+        }
+    }
+}
